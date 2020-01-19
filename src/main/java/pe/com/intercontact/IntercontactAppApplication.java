91261b2df9b5498ec8a8a4349916226571ff2aad
Jorge Luis Alvarez Alvarez <jorge.luis.alvarez.alvarez@everis.com>
2020-01-19 12:19:24 -0500

    chore: Create scaffolding

---
+++ src/main/java/pe/com/intercontact/IntercontactAppApplication.java
@@ -0,0 +1,13 @@
+package pe.com.intercontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercontactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercontactAppApplication.class, args);
+	}
+
+}
